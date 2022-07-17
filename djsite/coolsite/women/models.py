bb4669ada3db5d8478f0ef464ccd052a0807ca14
lirostin <lirostin@gmail.com>
2022-07-17 11:09:21 +0300

    5. CRUD - basics of ORM for working with models

--- djsite/coolsite/women/models.py
+++ djsite/coolsite/women/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Women(models.Model):
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d")
