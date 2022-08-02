60f697137801c32d049c4fa56b00b371759c7585
lirostin <lirostin@gmail.com>
2022-08-02 21:29:59 +0300

    9. Creating links between models ForeignKey

--- djsite/coolsite/women/views.py
+++ djsite/coolsite/women/views.py
@@ -11,11 +11,14 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     # когда не помещается информация в одну строку создаётся специальный словарь
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu, 
-        'title': "Главная страница"
+        'title': "Главная страница",
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -37,6 +40,22 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+        
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu, 
+        'title': "Отображение по рубриками",
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 def pageNotFound(request, exception):
 	return HttpResponseNotFound('<h1>Страница не найдена</h1>')
